fab7cb5e2f53f1fd0b85298636200e689561941e
Wong Wei Lok <wongweilok.personal@zohomail.com>
2022-03-20 12:54:56 +0800

    Changes to dwm statusbar scripts
    - Add color outputs to dwm statusbar.
    - Add powerline effect to statusbar.
    - Add Inconsolata-dz font to fontconfig.
    - Replace normal dwmblocks with dwmblocks-async.

---
+++ suckless/dwmblocks/config.h
@@ -0,0 +1,16 @@
+/* dwmblocks-async */
+
+#define CMDLENGTH 60
+#define DELIMITER ""
+
+const Block blocks[] = {
+	BLOCK("arrow1",     0,     0),
+	BLOCK("internet",   5,     4),
+	BLOCK("arrow2",     0,     0),
+	BLOCK("battery",    5,     0),
+	BLOCK("arrow2",     0,     0),
+	BLOCK("volume",     0,    10),
+	BLOCK("arrow2",     0,     0),
+	BLOCK("clock",      60,    0),
+	BLOCK("arrow3",	    0,	   0)
+};
