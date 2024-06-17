20849821e80796de08c916781180f79f69e45d08
Wong Wei Lok <contact.weilok@skiff.com>
2024-06-17 11:16:44 +0800

    Add minimal dwmblocks config

---
+++ suckless/dwmblocks/minimal/config.h
@@ -0,0 +1,11 @@
+/* dwmblocks-async */
+
+#define CMDLENGTH 60
+#define DELIMITER "   "
+
+const Block blocks[] = {
+	BLOCK("sb-internet",   5,     4),
+	BLOCK("sb-battery",    5,     0),
+	BLOCK("sb-volume",     0,    10),
+	BLOCK("sb-time",      60,     0),
+};
