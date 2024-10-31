3090afd7cb711458e06112333624443864476834
Wong Wei Lok <contact.weilok@skiff.com>
2024-10-31 20:03:59 +0800

    Migrate more settings from Vimscript config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,9 +1,2 @@
-vim.g.mapleader = ","
-
 require("weilok.config")
 require("weilok.lazy")
-
--- Load theme
-vim.cmd.colorscheme("gruvbox")
-vim.cmd.highlight({"Normal", "guibg=NONE"})
-vim.cmd.highlight({"Normal", "ctermbg=NONE"})
--- .config/nvim/lua/weilok/config/settings.lua
+++ .config/nvim/lua/weilok/config/settings.lua
@@ -1,2 +1,13 @@
-vim.opt.number = true
-vim.opt.relativenumber = true
+-- Basic settings
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.mouse = "a"
+opt.hlsearch = false
+opt.clipboard = "unnamedplus"
+opt.termguicolors = true
+opt.splitbelow = true
+opt.splitright = true
+opt.tabstop = 4
+opt.shiftwidth = 4
