18aaa3d917e6669dd0d3898f4c6500388837e91c
Wong Wei Lok <dev@wongweilok.com>
2024-10-31 20:03:59 +0800

    Migrate more settings from Vimscript config

--- .config/nvim/lua/weilok/plugins/gruvbox.lua
+++ .config/nvim/lua/weilok/plugins/gruvbox.lua
@@ -1,9 +1,15 @@
+-- Gruvbox colorscheme
 return {
-	-- Gruvbox colorscheme
 	{
 		"ellisonleao/gruvbox.nvim",
 		lazy = false,
 		priority = 1000,
-		opts = {},
+		opts = {
+			transparent_mode = true,
+		},
+		config = function(_, opts)
+			require("gruvbox").setup(opts)
+			vim.cmd([[colorscheme gruvbox]])
+		end
 	},
 }
