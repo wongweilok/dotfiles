ceee3495d5c2068661524a353c9d55de8752bad9
Wong Wei Lok <contact.weilok@skiff.com>
2024-11-01 17:25:19 +0800

    Change keymaps for nvim-lsp
    - Also change <leader> key to space.

--- .config/nvim/lua/weilok/plugins/lspconfig.lua
+++ .config/nvim/lua/weilok/plugins/lspconfig.lua
@@ -70,16 +70,12 @@ return {
 		local cmp = require("cmp")
 		cmp.setup({
 			mapping = {
-				['<S-Tab>'] = cmp.mapping.select_prev_item(),
-    				['<Tab>'] = cmp.mapping.select_next_item(),
-    				['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    				['<C-f>'] = cmp.mapping.scroll_docs(4),
-    				['<C-Space>'] = cmp.mapping.complete(),
-    				['<C-e>'] = cmp.mapping.close(),
-    				['<CR>'] = cmp.mapping.confirm {
-					behavior = cmp.ConfirmBehavior.Replace,
-					select = true,
-				},
+				['<C-j>'] = cmp.mapping.select_next_item(),
+				['<C-k>'] = cmp.mapping.select_prev_item(),
+				['<C-d>'] = cmp.mapping.scroll_docs(4),
+				['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    			['<C-Space>'] = cmp.mapping.complete(),
+				['<Tab>'] = cmp.mapping.confirm { select = true },
 			},
 			sources = {
 				{ name = 'nvim_lsp' }
