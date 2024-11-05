1153d199ee3e97a36e51ccb84b49a6ccd52c633c
Wong Wei Lok <contact.weilok@skiff.com>
2024-11-05 23:30:55 +0800

    Improve and optimise nvim plugin configs
    - Lazy load Telescope plugin using keymapping rather than on startup.

--- .config/nvim/lua/weilok/plugins/gruvbox.lua
+++ .config/nvim/lua/weilok/plugins/gruvbox.lua
@@ -3,9 +3,11 @@ return {
 	{
 		"ellisonleao/gruvbox.nvim",
 		priority = 1000,
+
 		opts = {
 			transparent_mode = true,
 		},
+
 		config = function(_, opts)
 			require("gruvbox").setup(opts)
 			vim.cmd([[colorscheme gruvbox]])
