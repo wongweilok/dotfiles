a3f88feddb353ca5dbdabc92a053a3b9d2bfdd19
Wong Wei Lok <dev@wongweilok.com>
2024-11-07 23:51:07 +0800

    Add goyo plugin and convert remaining config to lua

--- .config/nvim/lua/weilok/config/keymaps.lua
+++ .config/nvim/lua/weilok/config/keymaps.lua
@@ -1,1 +1,9 @@
 vim.g.mapleader = " "
+
+-- General keymaps
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>sc", ":setlocal spell! spelllang=en_gb<CR>")
+keymap.set("n", "S", ":%s//g<Left><Left>")
+keymap.set({ "n", "v" }, "<leader>j", '<Esc>/<++><CR>"_c4l')
+
---
+++ .config/nvim/lua/weilok/plugins/goyo.lua
@@ -0,0 +1,25 @@
+return {
+	"junegunn/goyo.vim",
+
+	keys = {
+		{ "<leader>gy", ":Goyo | set linebreak<cr>" }
+	},
+
+	init = function()
+		local goyo_group = vim.api.nvim_create_augroup("GoyoGroup", { clear = true })
+		vim.api.nvim_create_autocmd("User", {
+		    group = goyo_group,
+		    pattern = "GoyoEnter",
+		    callback = function()
+		        require("lualine").hide()
+		    end,
+		})
+		vim.api.nvim_create_autocmd("User", {
+		    group = goyo_group,
+		    pattern = "GoyoLeave",
+		    callback = function()
+		        require("lualine").hide({ unhide = true })
+		    end,
+		})
+	end
+}
