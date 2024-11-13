7e3d37bf7df405d990d63fab71571269e7623ba8
Wong Wei Lok <dev@wongweilok.com>
2024-11-13 18:57:11 +0800

    Minor changes to lualine and goyo plugin
    - Add spell check indication on the statusline.
    - No message print out when executing custom keybind.

--- .config/nvim/lua/weilok/plugins/goyo.lua
+++ .config/nvim/lua/weilok/plugins/goyo.lua
@@ -2,7 +2,7 @@ return {
 	"junegunn/goyo.vim",
 
 	keys = {
-		{ "<leader>gy", ":Goyo | set linebreak<cr>" }
+		{ "<leader>gy", ":Goyo | set linebreak<cr>", silent = true }
 	},
 
 	init = function()
--- .config/nvim/lua/weilok/plugins/lualine.lua
+++ .config/nvim/lua/weilok/plugins/lualine.lua
@@ -1,9 +1,20 @@
 -- Statusline by Lualine
+local function spell()
+	if vim.wo.spell == true then
+		return 'SPELL [' .. string.upper(vim.bo.spelllang) .. ']'
+	end
+	return ''
+end
+
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 
 	opts = {
 		options = { theme = "gruvbox-material" },
+
+		sections = {
+			lualine_x = { spell, 'encoding', 'fileformat', 'filetype' },
+		},
 	},
 }
